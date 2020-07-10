dca31cc0ebcb2c8c17055ad2be369243e87645f0
Rodrigo Zamperlini <rzamperlini@gmail.com>
2020-07-10 20:34:13 -0300

    Trying to organize the repository

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var circleCard3197AFBD9475490BBFF774FAFF879CF8: IVisualPlugin;
-export default circleCard3197AFBD9475490BBFF774FAFF879CF8;
--- .tmp/build/src/settings.d.ts
+++
@@ -1,9 +0,0 @@
-import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
-import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
-export declare class CircleSettings {
-    circleColor: string;
-    circleThickness: number;
-}
-export declare class VisualSettings extends DataViewObjectsParser {
-    circle: CircleSettings;
-}
